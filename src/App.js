aba01b97eb93a86309887504fce34190833da41c
gitpushjoe <josephobaraye@gmail.com>
2023-05-07 13:04:31 -0400

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import TCLLogo from './thechesslibrary.svg';
+
 function App() {
   return (
-    <h1>Hello, world!</h1>
+    <>
+    <div className="shadow-sm">
+    <div className="container col-10">
+       <nav className="navbar navbar-expand-md navbar-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+                <img src={TCLLogo} alt="The Chess Library" width="40" height="40" className="d-inline-block align-text-bottom pad1x" />
+                </a>
+            <a className="navbar-brand" href="/">The Chess Library</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Source Code</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Donate</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+       </nav>
+    </div>
+    </div>
+    </>
   );
 }
 
